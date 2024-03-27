30cd40673c674b92da8e75304f0c63f2ef902ed7
SarateanuAndrew <sarateanuandrei28@gmail.com>
2024-03-27 21:54:34 +0200

    Create scrape function of 999 request

--- go2web.py
+++ go2web.py
@@ -1,10 +1,27 @@
 import argparse
+from bs4 import BeautifulSoup
+import re
 import requests
 
 
 def make_request(url):
     response_of_url = requests.get(url)
     return response_of_url.text
+
+
+def search(term):
+    search_url = f"https://999.md/ro/search?query={term}"
+    response_of_999 = requests.get(search_url)
+    soup = BeautifulSoup(response_of_999.text, 'html.parser')
+
+    # Extract results from first search page
+    pattern = r'^/ro/\d{8}$'
+    count = 1
+    for link in soup.find_all('a'):
+        href = link.get('href')
+        if href and re.match(pattern, href) and len(link.text.strip()) > 1 and count <= 10:
+            print(f"{count}. {link.text} - https://999.md{link['href']}")
+            count += 1
 
 
 parser = argparse.ArgumentParser(
@@ -18,3 +35,5 @@ args = parser.parse_args()
 if args.url:
     response = make_request(args.url)
     print(response)
+elif args.search:
+    search(args.search)
