a8c37a5e898aacb6eeeb690325a9f94c1a0ccbb4
SarateanuAndrew <sarateanuandrei28@gmail.com>
2024-03-31 22:14:25 +0300

    Add gif with implementation

--- go2web.py
+++ go2web.py
@@ -5,16 +5,13 @@ import requests
 
 
 def make_request(url):
-    response_of_url = requests.get(url)
-    return response_of_url.text
+    return requests.get(url).text
 
 
 def search(term):
     search_url = f"https://999.md/ro/search?query={term}"
     response_of_999 = requests.get(search_url)
     soup = BeautifulSoup(response_of_999.text, 'html.parser')
-
-    # Extract results from first search page
     pattern = r'^/ro/\d{8}$'
     count = 1
     for link in soup.find_all('a'):
@@ -24,16 +21,15 @@ def search(term):
             count += 1
 
 
-parser = argparse.ArgumentParser(
-    description="CLI utility for making HTTP requests and displaying responses from a specific URL")
+parser = argparse.ArgumentParser()
 group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument("-u", "--url", help="Make an HTTP request to the specified URL")
-group.add_argument("-s", "--search", help="Make an HTTP request to search the term on 999.md platform")
+group.add_argument("-u", "--url_to_platform", help="HTTP request to one URL")
+group.add_argument("-s", "--search_word", help="HTTP request to search the term on 999.md")
 
 args = parser.parse_args()
 
-if args.url:
-    response = make_request(args.url)
+if args.url_to_platform:
+    response = make_request(args.url_to_platform)
     print(response)
-elif args.search:
-    search(args.search)
+elif args.search_word:
+    search(args.search_word)
